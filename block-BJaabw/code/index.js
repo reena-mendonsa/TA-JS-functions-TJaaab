9ed0b8215ae1f3101728d1c13d18544c0e14b86e
Reena Mendonsa <reenamendonsa@gmail.com>
2021-07-01 21:02:22 +0530

    Assignment2&3

--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -11,13 +11,17 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
-  // Your code
+function minToSec(minutes) {
+  var sec = minutes * 60;
+  return sec;
 }
 // - Execute the function with required parameter
-
+minToSec(50);
+minToSec(13);
+minToSec(2);
 /* 2. 
-Create a function named isInRange which validates whether a number n is exclusively within the bounds of lower and upper.
+Create a function named isInRange which validates whether a number n is
+ exclusively within the bounds of lower and upper.
 Return true and false based on that.
 
 The function accepts three parameter lower, upper and the number.
@@ -26,9 +30,14 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
-  // Your code
+function isInRange(lower,upper,num) {
+  if(num> lower && num < upper){
+    return true;
+  }
+  else return false;
 }
+console.log(isInRange(1, 20, 9)); // true
+console.log(isInRange(1, 10, 19)); // false
 // - Execute the function with required parameter
 
 /* 2. calculateBMI
@@ -36,9 +45,11 @@ function isInRange() {
 
 Take two arguments weight and height and return `Underweight`, `Normal` etc based on data.
 
-Body mass index(BMI) is calculated as follows: bmi = weight / (height x height). Write a function which calculates bmi.
+Body mass index(BMI) is calculated as follows: bmi = weight / (height x height).
+ Write a function which calculates bmi.
 BMI is used to broadly define different weight groups.
-Check if a person is underweight, normal, overweight or obese based the information given below.
+Check if a person is underweight, normal, overweight or obese based the 
+information given below.
 
 These are the conditions:
 
@@ -49,13 +60,32 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
-  // Your code
+function calculateBMI(height,weight) {
+  // var bmi = weight /(height * height);
+  let bmi = (weight / ((height * height) 
+                            / 10000)).toFixed(2);
+  console.log(bmi);
+  switch(true){
+    case bmi<18.5 : return 'underweight';
+    break;
+    case bmi >18.5 && bmi<24.9 : 
+    return 'Normal weight';
+    break;
+    case bmi>25 && bmi < 29.9 :
+      return 'OverWeight';
+      break;
+    case bmi > 30: 
+      return 'Obese';
+    default:
+      return 'Not A valid weight';
+  }
 }
+calculateBMI(155,100);
 
 /* 3. appropiateDrinks
 
-Create a function that take the age are return the appropiate drink based on these conditions:
+Create a function that take the age are return the appropiate drink based on
+ these conditions:
 
 - Under 14 years old — return "drink fruit juice"
 - Under 18 years old — return "drink soda"
@@ -64,14 +94,32 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
-  // Your code
-}
+function appropiateDrinks(age) {
+  switch (true){
+    case age <14 : 
+      return'drink fruit juice';
+      break;
+    case age> 15 && age <18 :
+      return 'drink soda';
+      break;
+    case age>18 && age <21 :
+      return 'drink fruit-flavoured vodka';
+      break;
+    case age>21 :
+      return 'drink throat iercing vodka';
+      break;
+    default:
+      return 'not a valid age';
 
+  }
+}
+appropiateDrinks(20);
 /* 4. Add two numers or string
 
-Write a function that accepts two numbers or string and returns the sum of the numbers and concatenation of the strings.
-Twist is when user passes anything other than number, or string value you should be able to handle that.
+Write a function that accepts two numbers or string and returns the sum of the numbers 
+and concatenation of the strings.
+Twist is when user passes anything other than number, or string value you should
+ be able to handle that.
 
 - If both values are number add them
 - If both values are string concat theme
@@ -79,8 +127,24 @@ Twist is when user passes anything other than number, or string value you should
 
 */
 
-function sum() {
-  // Your code
+function sum(value1 ,value2) {
+  var sumOfValues;
+//  if ( (typeof(value1) && typeof(value2) == "number") || (typeof(value1) && typeof (value2)== "string"))
+   if(typeof(value1)==typeof(value2))
+  {
+    if(typeof(value1)&& typeof(value2)=="number")
+    {
+      sumOfValues = value1+value2;
+      return sumOfValues;
+    }
+    else if( typeof(value1)&& typeof(value2)=="string")
+    {
+       return value1 + value2;
+    }
+  }
+  else{
+    return 'Enter valid values';
+  }
 }
 
 // Function Test
